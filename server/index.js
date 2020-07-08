f72fbc8abac5295e419a8f799042f049b9721aa7
Cole Finlayson <cole.fin8@gmail.com>
2020-07-08 12:44:40 -0600

    finished lecture

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
-const express = require('express');
-const session = require('express-session');
-const bcrypt = require('bcryptjs');
-const massive = require('massive');
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const massive = require("massive");
+const ctrl = require("./controller");
 
 const app = express();
 
@@ -14,13 +14,25 @@ app.use(
   session({
     secret: SESSION_SECRET,
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
   })
 );
 
-massive(CONNECTION_STRING).then(db => {
-  app.set('db', db);
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+}).then((db) => {
+  app.set("db", db);
+  console.log("database connected");
 });
+
+app.post("/auth/signup", ctrl.signup);
+app.post("/auth/login", ctrl.login);
+app.get("/auth/logout", ctrl.logout);
+app.get("/auth/user", ctrl.user);
 
 app.listen(SERVER_PORT, () => {
   console.log(`Listening on port: ${SERVER_PORT}`);
